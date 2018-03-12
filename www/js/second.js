7e01877a52238bf6c51684a41d89a79ad6364d25
luisgentil <luisgentil@yahoo.es>
2018-03-12 14:50:19 +0100

    Add files via upload

--- www/js/second.js
+++ www/js/second.js
@@ -10,6 +10,36 @@ var app = {
       mediaControl.srcObject = mediaStream;
       mediaControl.src = URL.createObjectURL(mediaStream);
     });
+      tracking.ColorTracker.registerColor('red', function(r, g, b) {
+    if (r > 100 && g < 50 && b < 50) {
+      return true;
+    }
+    return false;
+  });
+  var colors = new tracking.ColorTracker(['red','cyan', 'magenta', 'yellow']);
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+
+  colors.on('track', function(event) {
+  //  context.strokeStyle = '#fff';
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (event.data.length === 0) {
+      // No colors were detected in this frame.
+    } else {
+     // console.log(event.data);
+      event.data.forEach(function(rect) {
+        context.strokeStyle = rect.color;
+        context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+    //    console.log(rect.x, rect.y, rect.height, rect.width, rect.color);
+        context.font = '11px Helvetica';
+        context.fillStyle = rect.color;
+        context.fillText(rect.color, rect.x + rect.width + 5, rect.y + 11);
+      });
+    }
+  });
+
+  tracking.track('#myVideo', colors, {camera: true});
       },
 };
 
